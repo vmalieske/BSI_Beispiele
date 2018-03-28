7920794542ab2ce29eed1f86ccaf319bee3dbe39
xYoshimax <Vmaliesk@smail.uni-koeln.de>
2018-03-28 15:30:24 +0200

    Beispiel Html-Elemente Javascript

---
+++ Javascript/Javascript_dynamisch_Beispiel1/Test.js
@@ -0,0 +1,12 @@
+
+function loadMain() {
+    document.getElementById("vera").innerHTML += "<div id='Loadinganimation'></div><h1 id='ueberschrift_blau'>Titel</h1><p><div class='third'><img class='profile' src='Media/Profil.jpg' alt='Bild des Künstlers'></a></div><article class='down'><div class='second'><p>Willkommen auf dieser Homepage.<p>Im Folgenden stelle ich Ihnen was vor (Text folgt noch).<p>Viel Spaß!</p></div></article><article id=''></article></div>";
+    document.getElementById("daniel").innerHTML = "<div
+    id='Loadinganimation'></div><h1
+    id='ueberschrift_blau'>Titel</h1><p><div class='third'><img class='profile' src='Media/Profil.jpg' alt='Bild des Künstlers'></a></div><article class='down'><div class='second'><p>Willkommen auf dieser Homepage.<p>Im Folgenden stelle ich Ihnen was vor (Text folgt noch).<p>Viel Spaß!</p></div></article><article id=''></article></div>";
+
+
+}
+
+
+
