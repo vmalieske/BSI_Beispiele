bb155c3e31a5c30a6dd1e9784235cd98542268ef
xYoshimax <Vmaliesk@smail.uni-koeln.de>
2018-03-29 10:33:03 +0200

    Update Menue

--- Javascript/Javascript_dynamisch_Beispiel1/Test.js
+++ Javascript/Javascript_dynamisch_Beispiel1/Test.js
@@ -1,11 +1,14 @@
 
 function loadMain() {
-    document.getElementById("vera").innerHTML += "<div id='Loadinganimation'></div><h1 id='ueberschrift_blau'>Titel</h1><p><div class='third'><img class='profile' src='Media/Profil.jpg' alt='Bild des Künstlers'></a></div><article class='down'><div class='second'><p>Willkommen auf dieser Homepage.<p>Im Folgenden stelle ich Ihnen was vor (Text folgt noch).<p>Viel Spaß!</p></div></article><article id=''></article></div>";
-    document.getElementById("daniel").innerHTML = "<div
+    document.getElementById("content").innerHTML = "<div id='Loadinganimation'></div><h1 id='ueberschrift_blau'>Titel</h1><p><div class='third'><img class='profile' src='Media/Profil.jpg' alt='Bild des Künstlers'></a></div><article class='down'><div class='second'><p>Willkommen auf dieser Homepage.<p>Im Folgenden stelle ich Ihnen was vor (Text folgt noch).<p>Viel Spaß!</p></div></article><article id=''></article></div>";
+/*    document.getElementById("daniel").innerHTML = "<div
     id='Loadinganimation'></div><h1
-    id='ueberschrift_blau'>Titel</h1><p><div class='third'><img class='profile' src='Media/Profil.jpg' alt='Bild des Künstlers'></a></div><article class='down'><div class='second'><p>Willkommen auf dieser Homepage.<p>Im Folgenden stelle ich Ihnen was vor (Text folgt noch).<p>Viel Spaß!</p></div></article><article id=''></article></div>";
+    id='ueberschrift_blau'>Titel</h1><p><div class='third'><img class='profile' src='Media/Profil.jpg' alt='Bild des Künstlers'></a></div><article class='down'><div class='second'><p>Willkommen auf dieser Homepage.<p>Im Folgenden stelle ich Ihnen was vor (Text folgt noch).<p>Viel Spaß!</p></div></article><article id=''></article></div>";*/
+    
+}
 
-
+function loadCurriculum() {
+    document.getElementById("content").innerHTML = "<h1>Lebenslauf</h1><p>Hier steht mein Lebenslauf...</p>";
 }
 
 
