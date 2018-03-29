5355d8d8307c503fcd7c3eb50c31ec564fbe2eeb
xYoshimax <Vmaliesk@smail.uni-koeln.de>
2018-03-29 13:15:54 +0200

    Ajax - Dynamischer Inhalt

---
+++ Dynamik_mit_Ajax/Funktions.js
@@ -0,0 +1,21 @@
+function loadMain() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("content").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "home.txt", true);
+    xhttp.send();
+}
+
+function loadCurriculum() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("content").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "curriculum.txt", true);
+    xhttp.send();
+}
